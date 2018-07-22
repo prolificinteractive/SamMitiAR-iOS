432cfbf9a74fa59a30679ca07a64d4241f393995
V Jinangkul <v@prolificinteractive.com>
2018-07-22 06:25:56 -0400

    renaming

--- Examples/SamMiti Example - Multiple Placing/AppDelegate.swift
+++ Examples/SamMiti Example - Multiple Placing/AppDelegate.swift
@@ -7,18 +7,15 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
-        
+        // Override point for customization after application launch.
         return true
     }
 
